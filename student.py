8c52d5ae000d8fbc17ef2f613711dbec5c1240f2
mariaeduardamesquita <csa16-6235@colegiosantoantonio.com.br>
2025-09-02 15:19:08 -0300

    Update student.py

--- student.py
+++ student.py
@@ -3,11 +3,11 @@ impares=[]
 for i in numeros[-1::-2]:
   impares.append(int(i))
 pares=[]
-for p in numeros[-2::-2]:
-  if 2*int(p)<10:
-    pares.append(2*int(p))
+for i in numeros[-2::-2]:
+  if 2*int(i)<10:
+    pares.append(2*int(i))
   else:
-    pares.append(2*int(p)-10-1)
+    pares.append(2*int(i)-10-1)
 soma = sum(pares)+sum (impares)
 if int(soma/10)== soma/10:
   print ("Cartão válido")
